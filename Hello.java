0422f23ca1facd055ddcec39f4b0aa0cdfde8a9d
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-08-27 14:02:42 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,18 +3,22 @@
 public class Hello {
 	public printSTMT(){
 
-
+	
+	//always true
 	if(true){
 		logger.info("Always true");
 	}
 
+	//Sys out 
 	System.out.println("before");
 	logger.info("HellocWorld!!");
 	  try{
-		System.out.println("After");
+		logger.info("After");
 	  }catch(Exception e){
 		logger.error("error:",e);
 	  }
+	
+	int i = 0;
   }
 
 }
